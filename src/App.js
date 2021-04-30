882d1a8798a1c502a53d2944b1d16ccf67c02ecb
01F0 <01F0@users.noreply.github.com>
2021-04-30 21:21:27 +0200

    Added Elasticsearch server ping.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import elasticsearch from 'elasticsearch';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+let client = new elasticsearch.Client({
+  host: process.env.ELASTIC_IP_AND_PORT,
+  log: 'trace',
+  httpAuth: process.env.ELASTIC_USER_AND_PASSWORD,
+  apiVersion: '7.2', // use the same version of your Elasticsearch instance
+});
+
+class App extends Component
+{
+
+  render()
+  {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+        </header>
+      </div>
+    );
+  }
+  componentDidMount()
+  {
+    client.ping({
+      // ping usually has a 3000ms timeout
+      requestTimeout: 1000
+    }, function (error)
+    {
+      if (error)
+      {
+        console.trace('elasticsearch cluster is down!');
+      } else
+      {
+        console.log('All is well');
+      }
+    });
+  }
 
+}
 export default App;
