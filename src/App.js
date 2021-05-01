05dfa7727b60fe64adde4eed9d371eefa34bdcbe
01F0 <01F0@users.noreply.github.com>
2021-05-01 11:41:54 +0200

    Removed React starter stuff.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import elasticsearch from 'elasticsearch';
 import { Component } from 'react';
 
 
 let client = new elasticsearch.Client({
-  host: process.env.ELASTIC_IP_AND_PORT,
+  host: process.env.REACT_APP_ELASTIC_IP_AND_PORT,
   log: 'trace',
-  httpAuth: process.env.ELASTIC_USER_AND_PASSWORD,
+  httpAuth: process.env.REACT_APP_ELASTIC_USER_AND_PASSWORD,
   apiVersion: '7.2', // use the same version of your Elasticsearch instance
 });
 
 class App extends Component
 {
-
   render()
   {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+  
       </div>
     );
   }
